d644ff3b49f1a35641b17be5138853f0768b6af4
Sonryu <watsonvilar@gmail.com>
2025-03-15 17:39:56 -0300

    Foicriado e corrigido o rastreio do controle, agora há duas formas de rastreio e pode ser escolhida a que for masi compativel com o proposito requerido

--- Rastreio analogico DualShock4 com pygame para automação.py
+++ Rastreio analogico DualShock4 com pygame para automação.py
@@ -33,15 +33,16 @@ while RODANDO:
     eixo_y = controle.get_axis(1) #Cima-Baixo
 
     #ajuste de movimentação
-    DEADZONE = 0.1
+    DEADZONE = 0.1 #Para retirar valores indesejados.
 
-    if abs(eixo_x) > DEADZONE:
+    if abs(eixo_x) > DEADZONE: #Se for detectado movimento dentro do eixo X, o circulo da animação vai se mover em funcao do X
         pos_x += int(eixo_x * velocidade)
+    else: pos_x = int((eixo_x * velocidade) + (LARGURA/2)) #Se o analogico for solto, o circuo voltara para o centro da tela, que nesse caso, o X dele é a metade do tamanho da janela
 
-    if abs(eixo_y) > DEADZONE:
+    if abs(eixo_y) > DEADZONE: #Se for detectado movimento dentro do eixo Y, o circulo da animação vai se mover em funcao do Y
         pos_y += int(eixo_y * velocidade)
-
-    #limitando tela
+    else: pos_y = int((eixo_y * velocidade) + (ALTURA/2)) #Se o analogico for solto, o circuo voltara para o centro da tela, que nesse caso, o Y dele é a metade do tamanho da janela
+ 
     pos_x = max(RAIO, min(LARGURA - RAIO, pos_x))
     pos_y = max(RAIO, min(ALTURA - RAIO, pos_y))
 
